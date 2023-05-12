3bedbfea7c8fe61d700524d0dd5e1879441af39b
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-05-12 12:34:16 +0200

    A - Index server file & app config file

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import express from 'express'
+import morgan from 'morgan'
+import Database from './db'
+import { AppConfig } from './interfaces/enums'
+
+class app {
+  app: any
+  port: string = AppConfig.PORT
+  db = new Database()
+
+  constructor() {
+    this.app = express()
+
+    // Database connection
+    this.databaseConnection()
+
+    // Middlewares
+    this.middlewares()
+
+    // Routes
+    this.routes()
+  }
+
+  databaseConnection() {
+    this.db.dbConnection()
+  }
+
+  middlewares() {
+    this.app.use(express.json())
+    this.app.use(morgan('dev'))
+  }
+
+  routes() {
+    // this.app.use('/api', routes)
+  }
+
+  start() {
+    this.app.listen(this.port, () => {
+      console.log(`Server running at http://localhost:${this.port}`)
+    })
+  }
+}
+
+export default app
