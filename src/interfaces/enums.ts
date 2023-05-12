3917f49e718b2cbc6daf31cbecbe3444fd7b3c73
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-05-12 12:47:23 +0200

    U - Add enum responses

--- src/interfaces/enums.ts
+++ src/interfaces/enums.ts
@@ -5,3 +5,19 @@ export enum DatabaseURI {
 export enum AppConfig {
   PORT = '3000'
 }
+
+export enum ResponseStatus {
+  Ok = 200,
+  Created = 201,
+  BadRequest = 400,
+  NotFound = 404,
+  InternalServerError = 500
+}
+
+export enum ResponseMessage {
+  Ok = 'OK',
+  Created = 'CREATED',
+  BadRequest = 'BAD_REQUEST',
+  NotFound = 'NOT_FOUND',
+  InternalServerError = 'INTERNAL_SERVER_ERROR'
+}
