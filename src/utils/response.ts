aa54806033f18d25764f1d563d25e9a1b730387d
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-05-14 19:23:46 +0200

    A - Class ServerResponses with all used responses

---
+++ src/utils/response.ts
@@ -0,0 +1,57 @@
+import { ResponseMessage, ResponseStatus } from '../interfaces/enums'
+
+class ServerResponse {
+  okResponse(data: object) {
+    return {
+      status: ResponseStatus.Ok,
+      data
+    }
+  }
+
+  createdResponse(data: object) {
+    return {
+      status: ResponseStatus.Created,
+      data
+    }
+  }
+
+  deletedResponse(data: object) {
+    return {
+      status: ResponseStatus.Deleted,
+      data
+    }
+  }
+
+  badRequestResponse(data: object) {
+    return {
+      status: ResponseStatus.BadRequest,
+      data
+    }
+  }
+
+  notFoundResponse(data: object) {
+    return {
+      status: ResponseStatus.NotFound,
+      data
+    }
+  }
+
+  timeoutResponse(data: object) {
+    return {
+      status: ResponseStatus.Timeout,
+      data
+    }
+  }
+
+  errorResponse(err: any, service: string) {
+    return {
+      status: ResponseStatus.InternalServerError,
+      data: {
+        message: `${service} ${ResponseMessage.InternalServerError}`,
+        err
+      }
+    }
+  }
+}
+
+export default ServerResponse
