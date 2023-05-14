e7d3be8ce5769ddc4d221ce9aa33f6f8a853e3cb
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-05-14 18:40:00 +0200

    U - Get the last 5 feeds updated or saved of each newspaper in the DB

--- src/services/feed/getFeeds.service.ts
+++ src/services/feed/getFeeds.service.ts
@@ -1,13 +1,55 @@
 import { ResponseMessage, ResponseStatus } from '../../interfaces/enums'
+import { FeedInterface } from '../../interfaces/types'
+import Feed from '../../models/feed.model'
 
 class GetFeedsService {
+  private feeds: FeedInterface[] = []
+
   async getFeeds() {
     try {
+      this.feeds = await Feed.aggregate([
+        // Get the five front page stories of each newspaper
+        { $sort: { updatedAt: -1 } },
+        {
+          $group: {
+            _id: '$newspaper',
+            feeds: { $push: '$$ROOT' }
+          }
+        },
+        {
+          $project: {
+            newspaper: '$_id',
+            feeds: {
+              $slice: [
+                {
+                  $slice: ['$feeds', 5]
+                },
+                0,
+                5
+              ]
+            }
+          }
+        },
+        { $unwind: '$feeds' },
+        {
+          $project: {
+            _id: '$feeds._id',
+            text: '$feeds.text',
+            href: '$feeds.href',
+            newspaper: '$newspaper',
+            updatedAt: '$feeds.updatedAt'
+          }
+        },
+        { $sort: { updatedAt: -1 } },
+        { $limit: 10 }
+      ])
+
       return {
         status: ResponseStatus.Ok,
         data: {
+          status: ResponseStatus.Ok,
           type: ResponseMessage.Ok,
-          msg: 'getFeedsService()'
+          feeds: this.feeds
         }
       }
     } catch (err) {
