0463cb1cc4fc4c25c57074257550bfe6ad8c3393
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-05-14 18:54:04 +0200

    U - Get feed by ID

--- src/services/feed/getFeed.service.ts
+++ src/services/feed/getFeed.service.ts
@@ -1,14 +1,22 @@
 import { ResponseMessage, ResponseStatus } from '../../interfaces/enums'
+import { FeedInterface } from '../../interfaces/types'
+import Feed from '../../models/feed.model'
 
 class GetFeedService {
-  async getFeed(id: string) {
+  private feed!: FeedInterface
+
+  async getFeed(_id: string) {
     try {
+      this.feed =
+        (await Feed.findById(_id, { delete: false }, { _id: 0, __v: 0 })) ??
+        ({} as FeedInterface)
+
       return {
         status: ResponseStatus.Ok,
         data: {
           type: ResponseMessage.Ok,
           msg: 'getFeedService()',
-          id
+          feed: this.feed
         }
       }
     } catch (err) {
